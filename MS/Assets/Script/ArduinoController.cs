72f4c5f527278ebd6a068592ba520e82726c3e5e
AaronGao <aarongao1109@outlook.com>
2023-01-30 21:45:18 +0800

    20230130

---
+++ MS/Assets/Script/ArduinoController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArduinoController : MonoBehaviour
+{
+    SerialController serialController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+    }
+
+    // Update is called once per frame
+    public void ActivateClothMaterial()
+    {
+        serialController.SendSerialMessage("1");
+    }
+}
