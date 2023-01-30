1008277de6dc5d82d1e0b3adbdc18a8b115ad21b
AaronGao <aarongao1109@outlook.com>
2023-01-30 18:27:23 +0800

    20230130

---
+++ MS/Assets/Script/TransformLogger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformLogger : MonoBehaviour
+{
+    Transform loggedTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void LoggeCurrentTransform()
+    {
+        loggedTransform = GameObject.Find("Cube00").transform;
+    }
+}
